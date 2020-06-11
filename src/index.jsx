0149da71a6573699b984e4efe482c912486d34bb
floriansr <sueurfloriansf@gmail.com>
2020-06-11 12:58:31 +0200

    [fixup] moving provider from app.jsx to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,18 @@ import "./assets/scss/styles.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "redux/store";
+
+
 import App from "./App";
 
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-		<App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById("root")
 );
 
