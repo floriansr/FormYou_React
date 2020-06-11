89f09d5dd3cae9c85482f778c8662a92b0c08ef9
floriansr <sueurfloriansf@gmail.com>
2020-06-11 13:01:26 +0200

    [add] new checklog function for private roots

--- src/components/AuthRoute/index.jsx
+++ src/components/AuthRoute/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Route } from "react-router-dom";
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import Cookies from 'js-cookie'
+
+import { setConnexion, removeConnexion } from "../../redux";
+
 
 const AuthRoute = ({ component: Component, ...rest } : AuthRoute) => {
 
 	const logStatus = useSelector(state => state.log.log);
+	const dispatch = useDispatch();
+
+	const checkLog = () => (
+		Cookies.get('token') ? dispatch(setConnexion()) : dispatch(removeConnexion())
+	);
 
 	return (
 		<>
-			<Route {...rest} render={props => (logStatus) ? ( <Component {...props} />) : ( <p>This is a private page, go log in please.</p>)} />
+			<Route {...rest} render={props => (checkLog() && logStatus) ? ( <Component {...props} />) : ( <p>This is a private page, go log in please.</p>)} />
 		</>
 	);
 }
