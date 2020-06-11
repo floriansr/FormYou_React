ac4c253877fcaf036a597decd13a18b3a6e2e5f0
floriansr <sueurfloriansf@gmail.com>
2020-06-11 12:59:39 +0200

    [cleaned up] profile.jsx file

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,61 +1,12 @@
-import React, { useEffect } from "react";
-import Loader from "react-loader";
-import { Descriptions } from 'antd';
-
-// import ModificationMyProfile from "components/ModificationMyProfile"
-
-import { useSelector, useDispatch } from "react-redux"
-import Cookies from 'js-cookie'
-import { setProfile } from "../../redux";
+import React from "react";
 
 const Profile = () => {
-  const dispatch = useDispatch();
-  const token = Cookies.get('token')
 
-  useEffect(() => {
-
-    fetch('https://form-you-back.herokuapp.com/users/me.json', {
-      method: 'get',
-      headers: {
-        'Authorization': `Bearer ${token}`, 
-        'Content-Type': 'application/json'
-      },
-    })
-      .then(response => response.json())
-      .then(response => {
-        dispatch(setProfile(response))
-      })
-      .catch(error => console.log(error)); 
-  }, [dispatch, token]);
-
-  const myProfile = useSelector(state => state.user.data);
-
-  if (!myProfile) {
-    return (
-      <Loader />
-    )
-  }
-  
 	return (
 		<>
-      <Descriptions
-        title="My Profile"
-        bordered
-        column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
-      >
-        <Descriptions.Item label="Username">{myProfile.username}</Descriptions.Item>
-        <Descriptions.Item label="Email">{myProfile.email}</Descriptions.Item>
-        <Descriptions.Item label="ID">{myProfile.id}</Descriptions.Item>
-        <Descriptions.Item label="Amount">{myProfile.created_at}</Descriptions.Item>
-        <Descriptions.Item label="Description">
-          {myProfile.description}
-        </Descriptions.Item>
-      </Descriptions>
-      
+      <p>vkjsbgvjs;vbj</p>
 		</>
 	);
 };
-
-      // <ModificationMyProfile/>
 
 export default Profile;
